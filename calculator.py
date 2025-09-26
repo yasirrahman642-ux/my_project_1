13c5bd10bba3529597d0adb81b00f77581d031b3
yasirrahman642-ux <yasirrahman642@gmail.com>
2025-09-26 19:35:52 +0530

    calculator done

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
 print("calculator by yasir")
+num1 = float(input("enter number: "))
+operator = input('enter an operation(+-*/):')
+num2 = float(input("enter number: "))
+if operator == '+':
+    result = num1 + num2
+    print(result)
+elif operator == '-':
+    result = num1-num2
+    print(result)
+elif operator == '/':
+    result = num1 / num2
+    print(result)
+elif operator == '*':
+    result = num1 * num2
+    print(result)
+else:
+    print("Invalid number or the operator!! try again!")
